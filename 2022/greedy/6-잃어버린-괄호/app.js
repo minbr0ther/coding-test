a588e7b578993cd0cedcdbda08c85e5a2d1d81b5
minbr0ther <minbr0ther@naver.com>
2022-01-21 10:21:04 +0900

    greedy-6-잃어버린-암호

--- 2022/greedy/6-잃어버린-괄호/app.js
+++ 2022/greedy/6-잃어버린-괄호/app.js
@@ -2,21 +2,20 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-let n = +input[0];
-const arrA = input[1].split(' ').map((n) => +n);
-const arrB = input[2].split(' ').map((n) => +n);
+let string = input[0];
 
-arrA.sort((a, b) => a - b);
-
-let newArrB = [...arrB];
-const multipleArr = [];
-
-arrA.forEach((v) => {
-  let max = Math.max(...newArrB);
-
-  multipleArr.push(v * max);
-
-  newArrB = newArrB.filter((n) => n < max);
-});
-
-console.log(multipleArr.reduce((a, b) => a + b));
+console.log(
+  string
+    .split('-') // -를 기준으로 쪼갠다
+    .map((str) => {
+      if (str.split('').includes('+')) {
+        //+가 들어있는 str을 찾는다
+        // +기준으로 쪼개서 합을 더한뒤 반환한다
+        return str.split('+').reduce((a, b) => Number(a) + Number(b));
+      } else {
+        // str에 +가 없으면 그냥 반환한다
+        return Number(str);
+      }
+    }) // 최종적으로 남은 요소들을 - 연산 해준다
+    .reduce((a, b) => Number(a) - Number(b))
+);
