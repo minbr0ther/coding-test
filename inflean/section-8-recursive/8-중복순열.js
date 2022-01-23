f9d49786bd3bc28918afe13f80d5b9687001889b
minbr0ther <minbr0ther@naver.com>
2022-01-23 22:55:53 +0900

    8-8-중복순열 복습

--- inflean/section-8-recursive/8-중복순열.js
+++ inflean/section-8-recursive/8-중복순열.js
@@ -2,6 +2,18 @@ function solution(n, m) {
   let answer = [];
   let tmp = Array.from({ length: m }, () => 0);
 
+  const dfs = (L) => {
+    if (L === m) {
+      answer.push(tmp);
+    } else {
+      for (let i = 1; i <= n; i++) {
+        tmp[L] = i;
+        dfs(L + 1);
+      }
+    }
+  };
+  dfs(0);
+
   return answer;
 }
 
