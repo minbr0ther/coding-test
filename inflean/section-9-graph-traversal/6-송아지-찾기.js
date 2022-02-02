d0afb537f222b51154616f3d3bf6a0a2794eea13
minbr0ther <minbr0ther@naver.com>
2022-02-03 08:40:13 +0900

    6-송아지-찾기 2.0

--- inflean/section-9-graph-traversal/6-송아지-찾기.js
+++ inflean/section-9-graph-traversal/6-송아지-찾기.js
@@ -1,24 +1,21 @@
 function solution(s, e) {
-  let answer = 0;
-
   const check = new Set([s]);
   const distance = new Map([[s, 0]]);
   const queue = [s];
 
   while (queue.length) {
-    let x = queue.shift();
+    const x = queue.shift();
 
-    for (let nx of [x - 1, x + 1, x + 5]) {
+    for (const nx of [x - 1, x + 1, x + 5]) {
       if (nx === e) return distance.get(x) + 1;
-      if (nx >= 1 && nx <= 10000 && !check.has(nx)) {
+      if (!check.has(nx)) {
         check.add(nx);
         queue.push(nx);
         distance.set(nx, distance.get(x) + 1);
+        console.log(distance);
       }
     }
   }
-
-  return answer;
 }
 
 console.log(solution(8, 3));
